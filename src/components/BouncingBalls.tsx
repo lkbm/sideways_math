e7bb58af31cb1cdd375cb5806e8e8c74d72df762
Luca Kaceem Butler Masters <luca.masters@gmail.com>
2025-12-23 18:13:24 -0500

    Fix balls

--- src/components/BouncingBalls.tsx
+++ src/components/BouncingBalls.tsx
@@ -4,9 +4,15 @@
 export function BouncingBalls() {
   return (
     <div class="bouncing-balls-container" aria-hidden="true">
-      <div class="bouncing-ball ball-red" />
-      <div class="bouncing-ball ball-blue" />
-      <div class="bouncing-ball ball-yellow" />
+      {/* Green balls - bounce highest and fastest */}
+      <div class="bouncing-ball ball-green ball-green-1" />
+      <div class="bouncing-ball ball-green ball-green-2" />
+      {/* Red balls - moderate bounce */}
+      <div class="bouncing-ball ball-red ball-red-1" />
+      <div class="bouncing-ball ball-red ball-red-2" />
+      {/* Yellow balls - barely peek onto screen */}
+      <div class="bouncing-ball ball-yellow ball-yellow-1" />
+      <div class="bouncing-ball ball-yellow ball-yellow-2" />
     </div>
   );
 }
